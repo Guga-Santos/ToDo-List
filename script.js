cbae0ed483ae35427df8d7a218b828756de63f26
GugaSantos <guga.santos.guga@gmail.com>
2021-12-10 17:52:11 -0300

    Requisitos 06, 07 e 08

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function criarTarefa() {
   const divOl = document.querySelector('#lista-tarefas');
   const lista = document.createElement('li');
+  lista.classList.add('item');
 
   divOl.appendChild(lista);
   lista.innerText = document.querySelector('#texto-tarefa').value;
@@ -11,3 +12,12 @@ function criarTarefa() {
 const button = document.querySelector('#criar-tarefa');
 
 button.addEventListener('click', criarTarefa);
+
+function mudaBg(event) {
+  event.target.classList.add('alvo');
+  const alvo = document.querySelector('.alvo');
+  alvo.classList.remove('alvo');
+  event.target.classList.add('alvo');
+}
+
+document.querySelector('#lista-tarefas').addEventListener('click', mudaBg);
