16519f714cc3432101d75bc15313ef08b09df94f
GugaSantos <guga.santos.guga@gmail.com>
2021-12-10 22:23:13 -0300

    Alguns pontos dos requisitos bonus feitos

--- script.js
+++ script.js
@@ -30,6 +30,7 @@ const apagaButton = document.querySelector('#apaga-tudo');
 
 apagaButton.addEventListener('click', () => {
   divOl.innerHTML = '';
+  document.querySelector('ol').classList.add('alvo');
 });
 
 const finaliza = document.querySelector('#remover-finalizados');
@@ -39,4 +40,36 @@ finaliza.addEventListener('click', () => {
   while (finalizados.length > 0) {
     finalizados[0].parentNode.removeChild(finalizados[0]);
   }
+  document.querySelector('ol').classList.add('alvo');
 });
+
+// Retirei a ideia do bloco acima deste link:
+// https://qastack.com.br/programming/4777077/removing-elements-by-class-name
+
+// const salvaButton = document.querySelector('#salvar-tarefas');
+
+const praCima = document.querySelector('#mover-cima');
+const praBaixo = document.querySelector('#mover-baixo');
+
+function goUP() {
+  const selection = document.querySelector('.alvo');
+  const localTroca = document.querySelector('.alvo').previousElementSibling;
+  const reserva = selection.innerHTML;
+  selection.innerHTML = localTroca.innerHTML;
+  localTroca.innerHTML = reserva;
+  selection.classList.remove('alvo');
+  localTroca.classList.add('alvo');
+}
+
+function goDown() {
+  const selection = document.querySelector('.alvo');
+  const localTroca = document.querySelector('.alvo').nextElementSibling;
+  const reserva = selection.innerHTML;
+  selection.innerHTML = localTroca.innerHTML;
+  localTroca.innerHTML = reserva;
+  selection.classList.remove('alvo');
+  localTroca.classList.add('alvo');
+}
+
+praCima.addEventListener('click', goUP);
+praBaixo.addEventListener('click', goDown);
